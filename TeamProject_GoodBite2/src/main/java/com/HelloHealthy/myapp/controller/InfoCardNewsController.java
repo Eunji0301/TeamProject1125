4e11617ba3aec0b86e0a41c1a01367efc41478aa
KimSeOk <it5441233@gmail.com>
2025-01-07 10:53:42 +0900

    controller

--- TeamProject_GoodBite2/src/main/java/com/HelloHealthy/myapp/controller/InfoCardNewsController.java
+++ TeamProject_GoodBite2/src/main/java/com/HelloHealthy/myapp/controller/InfoCardNewsController.java
@@ -1,5 +1,39 @@
 package com.HelloHealthy.myapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/infoCardNews/")
 public class InfoCardNewsController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(InfoCardNewsController.class);
+	
+	@RequestMapping(value = "cardNewsList.do", method = RequestMethod.GET)
+	public String cardNewsList() {
+
+		logger.info("cardNewsList 들어옴");
+
+		return "WEB-INF/infoCardNews/cardNewsList";
+	}
+	
+	@RequestMapping(value = "cardNewsContents.do", method = RequestMethod.GET)
+	public String cardNewsContents() {
+
+		logger.info("cardNewsContents 들어옴");
+
+		return "WEB-INF/infoCardNews/cardNewsContents";
+	}
+	
+	@RequestMapping(value = "cardNewsWriteAndModify.do", method = RequestMethod.GET)
+	public String cardNewsWriteAndModify() {
+
+		logger.info("cardNewsWriteAndModify 들어옴");
+
+		return "WEB-INF/infoCardNews/cardNewsWriteAndModify";
+	}
 
 }
