9b45fc1bb1507e3ce50b0695c9204035d8556358
marine <kakasska1212@gmail.com>
2025-01-09 14:45:24 +0900

    user jspfolder name modify

--- TeamProject_GoodBite2/src/main/java/com/HelloHealthy/myapp/controller/UserController.java
+++ TeamProject_GoodBite2/src/main/java/com/HelloHealthy/myapp/controller/UserController.java
@@ -1,70 +1,130 @@
 package com.HelloHealthy.myapp.controller;
 
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+//import org.json.JSONArray;
+//import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.HelloHealthy.myapp.domain.UserVo;
+import com.HelloHealthy.myapp.service.UserService;
+import com.HelloHealthy.myapp.util.UserIp;
 
 @Controller
 @RequestMapping(value = "/")
 public class UserController {
-	private static final Logger logger = LoggerFactory.getLogger(FoodTrackerController.class);
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
-	@RequestMapping(value = "user/login.do", method = RequestMethod.GET)
+	@Autowired 
+	private UserIp userIp;
+
+	@Autowired 
+	private UserService userService;
+	
+	@RequestMapping(value = "user/userLogin.do", method = RequestMethod.GET)
 	public String login() {
 
 		logger.info("login 들어옴");
 
-		return "WEB-INF/user/login";
+		return "WEB-INF/user/userLogin";
 	}
 	
-	@RequestMapping(value = "user/join.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userJoin.do", method = RequestMethod.GET)
 	public String join() {
 
 		logger.info("join 들어옴");
 
-		return "WEB-INF/user/join";
+		return "WEB-INF/user/userJoin";
 	}
+
+//	@RequestMapping(value = "user/userJoinAction.do", method = RequestMethod.POST)
+//	public String joinAction(UserVo uv, HttpServletRequest request, RedirectAttributes rttr)throws IOException, Exception {
+//		logger.info("joinAction 들어옴");
+//		logger.info(uv.getuId());
+//		logger.info(uv.getuPwd());
+//		logger.info(uv.getuName());
+//		logger.info(uv.getuGender());
+//		logger.info(uv.getuBirth());
+//		logger.info(uv.getuPhone());
+//		logger.info(uv.getuNickName());
+//		logger.info(uv.getuMail());
+//		logger.info(uv.getuThumbnail());
+//		logger.info(uv.getuIp());
+//		logger.info(uv.getuTOSAgreement());
+//		logger.info(uv.getuPIPConsent());
+//		
+//
+//		uv.setuThumbnail("themnailBasic.png");
+//		String ip = userIp.getUserIp(request);
+//		uv.setuIp(ip);
+//		uv.setuRole("USER");
+//		
+//		int value = userService.userInsert(uv);
+//		
+//		String path = "";
+//		if (value == 1) {
+//			path = "redirect:/user/userLogin.do";
+//		} else {
+//			rttr.addFlashAttribute("msg", "입력이 잘못 되었습니다.");
+//			path = "redirect:/user/userJoin.do";
+//		}
+//		
+//		return path;
+//	}
 	
-	@RequestMapping(value = "user/foundId.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userFoundId.do", method = RequestMethod.GET)
 	public String foundId() {
 
 		logger.info("foundId 들어옴");
 
-		return "WEB-INF/user/foundId";
+		return "WEB-INF/user/userFoundId";
 	}
 	
-	@RequestMapping(value = "user/foundPwd.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userFoundPwd.do", method = RequestMethod.GET)
 	public String foundPwd() {
 
 		logger.info("foundPwd 들어옴");
 
-		return "WEB-INF/user/foundPwd";
+		return "WEB-INF/user/userFoundPwd";
 	}
 	
-	@RequestMapping(value = "user/searchId.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userSearchId.do", method = RequestMethod.GET)
 	public String searchId() {
 
 		logger.info("foundId 들어옴");
 
-		return "WEB-INF/user/searchId";
+		return "WEB-INF/user/userSearchId";
 	}
 	
-	@RequestMapping(value = "user/searchPwd.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userSearchPwd.do", method = RequestMethod.GET)
 	public String searchPwd() {
 
 		logger.info("searchPwd 들어옴");
 
-		return "WEB-INF/user/searchPwd";
+		return "WEB-INF/user/userSearchPwd";
 	}
 	
-	@RequestMapping(value = "user/changePwd.do", method = RequestMethod.GET)
+	@RequestMapping(value = "user/userChangePwd.do", method = RequestMethod.GET)
 	public String changePwd() {
 
 		logger.info("changePwd 들어옴");
 
-		return "WEB-INF/user/changePwd";
+		return "WEB-INF/user/userChangePwd";
 	}
 	
 	@RequestMapping(value = "common/footer.do", method = RequestMethod.GET)
--- TeamProject_GoodBite2/src/main/webapp/WEB-INF/user/userSearchPwd.jsp
+++ TeamProject_GoodBite2/src/main/webapp/WEB-INF/user/userSearchPwd.jsp
@@ -4,30 +4,29 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>GoodBite Search Id</title>
+<title>GoodBite Search Pwd</title>
 <link rel="stylesheet" type="text/css" href="../resources/css/bootstrap-5.3.3-dist/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="../resources/css/bootstrap-5.3.3-dist/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="../resources/css/user.css">
-
 </head>
 <body>
 <%@ include file="../common/headerBefore.jsp"%>
     <main id="search">
         <div class="searchInner">
-            <h1 class="title">아이디 찾기</h1>
+            <h1 class="title">비밀번호 찾기</h1>
             <div class="searchBorder p-4">
             	<div class="text-center my-3">
             		<img src="../resources/img/common/titlelogoimg.png">
-            		<div class="mt-2">* 아래에 정보를 입력해 주세요. *</div>
+            		<div class=" mt-2">* 아래에 정보를 입력해 주세요. *</div>
             	</div>
 	            <form class="search-form">
 	                <input type="text" placeholder="아이디" required>
-	                <input type="password" placeholder="비밀번호" required>
-	                <button type="button" class="searchIdButton mt-3 " >아이디 찾기</button>
+	                <input type="text" placeholder="이메일 주소" required>
+	                <button type="button" class="searchIdButton mt-3 ">비밀번호 찾기</button>
 	                <hr>
 	                <div class=" mt-4 ">
-		                <div>비밀번호를 찾고싶다면? <a href="#">비밀번호 찾기</a></div>
-		                <div>아이디를 기억하셨다면? <a href="#">로그인 하기</a></div>
+		                <div>아이디를 찾고싶다면? <a href="#">아이디 찾기</a></div>
+		                <div>비밀번호를 기억하셨다면? <a href="#">로그인 하기</a></div>
 	                </div>
 	            </form>
             </div>
@@ -35,6 +34,7 @@
     </main>
 <%@ include file="../common/footer.jsp"%>
 </body>
-<script type="text/javascript" src="../resources/css/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
-<script type="text/javascript" src="../resources/css/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../resource/css/bootstrap-5.3.3-dist/js/bootstrap.js"></script>
+<script type="text/javascript" src="../resource/css/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+
 </html>
